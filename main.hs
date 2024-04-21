aa2b14420e77677b2577011fb1fe271e119ccf26
Miguel Castro <69170175+Miguel-de-Castro@users.noreply.github.com>
2024-04-21 22:08:11 +0000

    add ex3 ex4

--- main.hs
+++ main.hs
@@ -11,19 +11,63 @@ dec2Bin dec bits
     where
         binRep = dec2BinHelper dec
 
+-- 3
+bincompl2dec :: [Int] -> Int
+bincompl2dec [] = -1
+bincompl2dec xs = if (head xs) == 0 then bin2Dec xs else -bin2Dec (bincompl2Negative xs)
+
+-- 4
+dec2bincompl :: Int -> Int -> [Int]
+dec2bincompl x y = if x < 0 then bincompl2Negative (dec2Bin (abs x) y) else dec2Bin (abs x) y 
+
+
+-- 5
+somarbin :: [Int] -> [Int] -> Int -> [Int]
+somarbin [] [] 0 = []
+
+
 -- Aux Func
 dec2BinHelper :: Int -> [Int]
 dec2BinHelper 0 = [0]
 dec2BinHelper 1 = [1]
 dec2BinHelper n = dec2BinHelper (n `div` 2) ++ [n `mod` 2]
 
+xor :: Bool -> Bool -> Bool
+xor True False = True
+xor False True = True
+xor _ _ = False
 
--- 3
+intToBool :: Int -> Bool
+intToBool 0 = False
+intToBool 1 = True
 
+boolToInt :: Bool -> Int
+boolToInt False = 0
+boolToInt True = 1
 
+xorOfBins :: [Int] -> [Int] -> [Int]
+xorOfBins [] [] = []
+xorOfBins (x:xs) (y:ys) = boolToInt (xor (intToBool x) (intToBool y)) : xorOfBins xs ys
 
--- 4
+arrayOfOne :: Int -> [Int]
+arrayOfOne n = [1 | x <- [1..n]]
 
+bin :: [Int] -> [Int]
+bin xs = xorOfBins xs (arrayOfOne (length xs))
 
+arrayLastOne :: Int -> [Int]
+arrayLastOne n = [if x == n then 1 else 0 | x <- [1..n]]
 
--- 5
+addOfBins :: [Int] -> [Int] -> Int -> [Int]
+addOfBins [] [] 0 = []
+addOfBins [] [] 1 = []
+addOfBins (x:xs) (y:ys) z | (x + y + z == 3) = z : addOfBins xs ys 1
+                          | (x + y + z == 2) = 0 : addOfBins xs ys 1
+                          | x == y = 0 + z : addOfBins xs ys 0
+                          | otherwise = 1 : addOfBins xs ys 0
+
+bincompl2Negative :: [Int] -> [Int]
+bincompl2Negative xs = reverse (addOfBins (reverse (bin xs)) (reverse (arrayLastOne (length xs))) 0)
+
+-- preciso validar se o número é negativo ou não
+-- dec2bincompl :: [Int] -> Int -> [Int]
